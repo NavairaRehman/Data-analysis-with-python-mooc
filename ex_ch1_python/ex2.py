d3918136eca5ed2ab76a61be27df7ae2398d87df
Navaira Rehman <nrehman.bese21seecs@student.nust.edu.pk>
2024-01-25 16:37:34 +0500

    feat : Add solution for exercise 1.2 describing the use of input statement

---
+++ ex_ch1_python/ex2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Exercise 2:
+
+The program should ask the user for an input, and then print an answer as the examples below show.
+
+What country are you from? Sweden
+I have heard that Sweden is a beautiful country.
+
+'''
+
+def main():
+    country = input("What country are you from? ")
+    print("I have heard that",country, "is a beautiful country.")
+
+
+if __name__ == "__main__":
+    main()
